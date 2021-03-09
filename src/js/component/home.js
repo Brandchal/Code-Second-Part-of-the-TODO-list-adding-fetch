a4cf0fbc6d5a4781d9dcba1e5058568b4971ba4d
Brandchal <Bchavarrial@hotmail.com>
2021-03-09 03:21:31 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,81 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export class Home extends React.Component {
+	constructor() {
+		super();
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		this.state = {
+			todos: [
+				{ done: false, title: "Hacer la cama", id: Math.random() * 10 },
+				{
+					done: false,
+					title: "Lavar mis manos",
+					id: Math.random() * 10
+				},
+				{ done: false, title: "Comer", id: Math.random() * 10 },
+				{
+					done: false,
+					title: "Pasear con el perro",
+					id: Math.random() * 10
+				}
+			],
+			taskInput: ""
+		};
+	}
+
+	formSubmit(e) {
+		e.preventDefault();
+		this.setState({
+			todos: this.state.todos.concat([
+				{
+					title: this.state.taskInput,
+					done: false,
+					id: Math.random() * 10
+				}
+			]),
+			taskInput: ""
+		});
+		return false;
+	}
+
+	deleteTask(taskId) {
+		this.setState({
+			todos: this.state.todos.filter(task => task.id != taskId)
+		});
+	}
+
+	render() {
+		let tasksToRender = this.state.todos.map(task => {
+			return (
+				<li key={task.id}>
+					<span>
+						<button
+							className="destroy"
+							onClick={() => this.deleteTask(task.id)}>
+							<i className="fa fa-trash" />
+						</button>
+					</span>
+					<label>{task.title}</label>
+				</li>
+			);
+		});
+
+		return (
+			<div id="container">
+				<h1 className="todo-header">To do List</h1>
+				<form onSubmit={this.formSubmit.bind(this)}>
+					<input
+						autoFocus={true}
+						className="addToDo"
+						placeholder="Que necesitas hacer?"
+						value={this.state.taskInput}
+						onChange={evt =>
+							this.setState({ taskInput: evt.target.value })
+						}
+					/>
+				</form>
+				<ul className="todo-list">{tasksToRender}</ul>
+			</div>
+		);
+	}
 }
