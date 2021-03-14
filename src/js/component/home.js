f57f193b278f4362644034adde71cc3be6175934
Brandchal <Bchavarrial@hotmail.com>
2021-03-14 04:58:27 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,15 @@
 import React from "react";
 
+const URL_USER = "https://assets.breatheco.de/apis/fake/todos/user/Brandchal";
+
 export class Home extends React.Component {
 	constructor() {
 		super();
 
 		this.state = {
-			notas: [
-				{ done: false, title: "Make the bed", id: Math.random() * 10 },
-				{
-					done: false,
-					title: "Wash my hands",
-					id: Math.random() * 10
-				},
-				{ done: false, title: "Eat", id: Math.random() * 10 },
-				{
-					done: false,
-					title: "Walk the dog",
-					id: Math.random() * 10
-				}
-			],
+			notas: [],
+			isLoading: false,
+			error: null,
 			taskInput: ""
 		};
 	}
@@ -31,18 +22,84 @@ export class Home extends React.Component {
 		}
 	}
 
+	componentDidMount() {
+		this.setState({ isLoading: true });
+		fetch(URL_USER, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error("Something went wrong ...");
+				}
+			})
+			.then(
+				data =>
+					data.map(todo => {
+						this.setState({
+							notas: this.state.notas.concat([
+								{
+									label: todo.label,
+									done: todo.done,
+									id: todo.id
+								}
+							])
+						});
+					}),
+				this.setState({ isLoading: false })
+			)
+			.catch(error => this.setState({ error, isLoading: false }));
+	}
+
 	formSubmit(e) {
 		e.preventDefault();
+		let newTodo = {
+			label: this.state.taskInput,
+			done: false,
+			id: Math.random() * 10
+		};
+
 		this.setState({
 			notas: this.state.notas.concat([
 				{
-					title: this.state.taskInput,
-					done: false,
-					id: Math.random() * 10
+					label: newTodo.label,
+					done: newTodo.done,
+					id: newTodo.id
 				}
 			]),
 			taskInput: ""
 		});
+
+		let notas = this.state.notas.concat([newTodo]);
+
+		if (notas.length == 1) {
+			fetch(URL_USER, {
+				method: "POST",
+				body: JSON.stringify(notas),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(response => response.json())
+				.catch(error => console.error("Error:", error))
+				.then(response => console.log("Success:", response));
+		} else {
+			fetch(URL_USER, {
+				method: "PUT",
+				body: JSON.stringify(notas),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(response => response.json())
+				.catch(error => console.error("Error:", error))
+				.then(response => console.log("Success:", response));
+		}
+
 		return false;
 	}
 
@@ -50,10 +107,44 @@ export class Home extends React.Component {
 		this.setState({
 			notas: this.state.notas.filter(task => task.id != taskId)
 		});
+
+		let notas = this.state.notas.filter(task => task.id != taskId);
+
+		if (notas.length >= 1) {
+			fetch(URL_USER, {
+				method: "PUT",
+				body: JSON.stringify(notas),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(response => response.json())
+				.catch(error => console.error("Error:", error))
+				.then(response => console.log("Success:", response));
+		} else {
+			fetch(URL_USER, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(response => response.json())
+				.catch(error => console.error("Error:", error))
+				.then(response => console.log("Success:", response));
+		}
 	}
 
 	render() {
-		let tasksToRender = this.state.notas.map(task => {
+		const { notas, isLoading, error } = this.state;
+		if (error) {
+			return <p>{error.message}</p>;
+		}
+
+		if (isLoading) {
+			return <p>Loading ...</p>;
+		}
+
+		let tasksToRender = notas.map(task => {
 			return (
 				<li key={task.id}>
 					<span>
@@ -63,16 +154,14 @@ export class Home extends React.Component {
 							<i className="fa fa-minus-square" />
 						</button>
 					</span>
-
-					<label>{task.title}</label>
+					<label>{task.label}</label>
 				</li>
 			);
 		});
 
 		return (
 			<div id="container">
-				<h1 className="header">To do List</h1>
-
+				<h1 className="todo-header">To do List</h1>
 				<form onSubmit={this.formSubmit.bind(this)}>
 					<input
 						autoFocus={true}
